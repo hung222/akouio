10afe98cb96c17fc07d833a382ee11cb8db68b9c
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-12 22:38:04 +0700

    Update welcome.blade.php

--- tlww/resources/views/welcome.blade.php
+++ tlww/resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
-{!! '<b>ghghghgh</b>' !!}
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Blade template</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h1>Chào mừng các bạn đã đến websitegameol</h1>
+</body>
+</html>
